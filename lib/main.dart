23cd4da40401e5aa78cf536fc57a7360b1390d49
Marta Odrzywolek <odrzywolek.m@gmail.com>
2024-10-09 11:19:35 +0200

    add keyboard type to email textfield

--- lib/main.dart
+++ lib/main.dart
@@ -51,6 +51,9 @@ class _HomePageState extends State<HomePage> {
         children: [
           TextField(
             controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
             decoration: const InputDecoration(
               hintText: 'Enter your email here'
             ),
@@ -58,17 +61,19 @@ class _HomePageState extends State<HomePage> {
           TextField(
             controller: _password,
             obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
             decoration: const InputDecoration(
               hintText: 'Enter your password here'
             ),
           ),
           TextButton(onPressed: () async {
-            final email = _email.text;
-            final password = _password.text;
-            final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-              email: email, 
-              password: password);
-              print(userCredential);
+            // final email = _email.text;
+            // final password = _password.text;
+            // final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+            //   email: email, 
+            //   password: password);
+            //   print(userCredential);
           }, 
           child: const Text('Register')
           )
