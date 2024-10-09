d8619a2eaa33cf680d9e1b3536f859caa3c40fe9
Marta Odrzywolek <odrzywolek.m@gmail.com>
2024-10-09 11:26:38 +0200

    fix firebase error

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mynotes/firebase_options.dart';
 
 void main() {
   runApp(
@@ -68,6 +70,10 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           TextButton(onPressed: () async {
+            await Firebase.initializeApp(
+              options: DefaultFirebaseOptions.currentPlatform,
+            );
+
             final email = _email.text;
             final password = _password.text;
             final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
