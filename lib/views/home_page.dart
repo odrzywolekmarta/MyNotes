e914b37defcea880e20980bface0535705385409
Marta Odrzywolek <odrzywolek.m@gmail.com>
2024-10-15 13:14:01 +0200

    fix issue

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -15,13 +17,17 @@ class HomePage extends StatelessWidget {
         builder:(context, snapshot) {
           switch (snapshot.connectionState) {
           case ConnectionState.done:
-          // final user = FirebaseAuth.instance.currentUser;
-          // if (user?.emailVerified ?? false) {
-          // return Text('Done');
-          // } else {
-          //   return const VerifyEmailView();
-          // }
-          return const LoginView();
+          final user = FirebaseAuth.instance.currentUser;
+          if (user != null) {
+            if (user.emailVerified) {
+              print('Email is verified');
+            } else {
+              return const VerifyEmailView();
+            }
+          } else {
+            return const LoginView();
+          }
+          return const Text('Done');
         default: 
         return const CircularProgressIndicator();
           }
