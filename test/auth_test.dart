64ee9381912810a97bfb2da3d92c667da7c19ab9
Marta Odrzywolek <odrzywolek.m@gmail.com>
2024-10-23 10:50:37 +0200

    add mock auth provider

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -1,3 +1,64 @@
+import 'package:mynotes/services/auth_exceptions.dart';
+import 'package:mynotes/services/auth_user.dart';
+import 'package:mynotes/services/firebase_auth_provider.dart';
 import 'package:test/test.dart';
 
 void main() {}
+
+class NotInitializedException implements Exception {}
+
+class MockAuthProvider implements AuthProvider {
+  AuthUser? _user;
+  var _isInitialized = false;
+  bool get isInitialized => _isInitialized;
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) async {
+    if (!isInitialized) throw NotInitializedException();
+    await Future.delayed(const Duration(seconds: 1));
+    return logIn(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  // TODO: implement currentUser
+  AuthUser? get currentUser => _user;
+
+  @override
+  Future<void> initialize() async {
+    await Future.delayed(Duration(seconds: 1));
+    _isInitialized = true;
+  }
+
+  @override
+  Future<AuthUser> logIn({required String email, required String password}) {
+    if (!isInitialized) throw NotInitializedException();
+    if (email == 'test@gmail.com') throw UserNotFoundAuthException();
+    if (password == 'test123') throw WrongPasswordAuthException();
+    const user = AuthUser(isEmalVerified: false);
+    _user = user;
+    return Future.value(user);
+  }
+
+  @override
+  Future<void> logOut() async {
+    if (!isInitialized) throw NotInitializedException();
+    if (_user == null) throw UserNotFoundAuthException();
+    await Future.delayed(Duration(seconds: 1));
+    _user == null;
+  }
+
+  @override
+  Future<void> sendEmailVerification() async {
+    if (!isInitialized) throw NotInitializedException();
+    final user = _user;
+    if (user == null) throw UserNotFoundAuthException();
+    const newUser = AuthUser(isEmalVerified: true);
+    _user = newUser;
+  }
+}
